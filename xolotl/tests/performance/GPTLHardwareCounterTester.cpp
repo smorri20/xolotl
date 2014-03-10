4f0311123b71f022b8d1f34ac076192a2ff83822
cjernigan <cjernigan@mycompany.com>
2014-03-10 15:53:47 +0000

    Updated GPTLHardwareCounterTester.  CLJ 20140310
    git-svn-id: file:///Users/MacAir/Dropbox/xolotl-cleaned/branches/performance_infrastructure_branch@292 72105f29-0fe1-43bd-9363-b1e281e962a2

--- xolotl/tests/performance/GPTLHardwareCounterTester.cpp
+++ xolotl/tests/performance/GPTLHardwareCounterTester.cpp
@@ -12,8 +12,11 @@
 using namespace std;
 using namespace xolotlPerf;
 
-const std::vector<HardwareQuantities> test_hardwareQuantities =
-	{L1_CACHE_MISS,L2_CACHE_MISS,L3_CACHE_MISS,BRANCH_MISPRED,TOTAL_CYCLES,TOTAL_INSTRUC,FLPT_INSTRUC};
+//const std::vector<HardwareQuantities> test_hardwareQuantities ={L1_CACHE_MISS,L2_CACHE_MISS,
+//		L3_CACHE_MISS,BRANCH_MISPREDICTIONS,TOTAL_CYCLES,TOTAL_INSTRUCTIONS,FLPT_INSTRUCTIONS};
+
+const std::vector<HardwareQuantities> test_hardwareQuantities ={L1_CACHE_MISS,
+		TOTAL_CYCLES,TOTAL_INSTRUCTIONS,FLPT_INSTRUCTIONS};
 
 /**
  * This suite is responsible for testing the HardwareCounter.
@@ -35,12 +38,13 @@ BOOST_AUTO_TEST_CASE(check_getHardwareQuantities) {
 
 	GPTLHardwareCounter tester("test",test_hardwareQuantities);
 
-	BOOST_TEST_MESSAGE("\n" << "GPTLHardwareCounter Message: \n" << "test_hardwareQuantities = ");
+	BOOST_TEST_MESSAGE("\n" << "GPTLHardwareCounter Message: \n"
+							<< "test_hardwareQuantities = ");
 	for(unsigned i = 0; i < test_hardwareQuantities.size(); i++){
 
 		BOOST_TEST_MESSAGE(" " << tester.getHardwareQuantities()[i] << " ");
 	}
-
+	
 #if READY
 	for(unsigned i = 0; i < test_hardwareQuantities.size(); i++){
 
@@ -48,7 +52,52 @@ BOOST_AUTO_TEST_CASE(check_getHardwareQuantities) {
 		BOOST_REQUIRE_EQUAL(test_hardwareQuantities[i], tester.getHardwareQuantities()[i]);
 	}
 #endif // READY
+}	
 }
 
+BOOST_AUTO_TEST_CASE(check_getValues) {
+
+	GPTLsetoption(PAPI_L1_TCM, 1);
+//	GPTLsetoption(PAPI_L2_TCM, 1);
+//	GPTLsetoption(PAPI_L3_TCM, 1);
+//	GPTLsetoption(PAPI_BR_MSP, 1);
+	GPTLsetoption(PAPI_TOT_CYC, 1);
+	GPTLsetoption(PAPI_TOT_INS, 1);
+	GPTLsetoption(PAPI_FP_INS, 1);
+	GPTLsetoption(PAPI_FP_OPS, 1);
+	GPTLinitialize();
+
+	GPTLHardwareCounter tester("test",test_hardwareQuantities);
+	GPTLstart("test");
+	std::string testName = tester.getName();
+
+	double a;
+	for(unsigned i = 0; i < 4; i++)
+		a = (double)i/10;
+
+	BOOST_TEST_MESSAGE("\n" << "GPTLHardwareCounter Message: \n"
+							<< "test_hardwareQuantities = ");
+
+	GPTLstop("test");
+
+	double val; // = 0.0;
+	int gret = GPTLget_eventvalue("test", "PAPI_FP_OPS", -1, &val);
+	if (gret < 0)
+	{
+		BOOST_TEST_MESSAGE("\n" << "GPTLget_eventvalue failed: " << gret);
+	}
+	else
+		BOOST_TEST_MESSAGE("\n" << "PAPI_FP_OPS = " << val);
+
+	for(unsigned i = 0; i < tester.getValues().size(); i++){
+
+		BOOST_TEST_MESSAGE(" " << tester.getHardwareQuantities()[i]
+		                       << " = " << tester.getValues()[i] << " ");
+	}
+
+//	GPTLpr(0);
+
+	GPTLfinalize();
+}
 
 BOOST_AUTO_TEST_SUITE_END()
