fcbae07072d4bb90e3fef24694b494e8f2be7f26
Sophie Blondel <sophie.blondel1@gmail.com>
2014-04-01 13:06:20 +0000

    Working version of the monitor refactoring. -mymonitor option does not write xolotl output files anymore, SB 20140401
    git-svn-id: file:///Users/MacAir/Dropbox/xolotl-cleaned/branches/visualization_implementation_branch@340 72105f29-0fe1-43bd-9363-b1e281e962a2

--- xolotl/xolotlSolver/Monitor.cpp
+++ xolotl/xolotlSolver/Monitor.cpp
@@ -11,7 +11,7 @@
 #include <vector>
 #include <memory>
 
-using namespace xolotlSolver;
+namespace xolotlSolver {
 
 /* ----- Error Handling Code ----- */
 
@@ -37,48 +37,15 @@ static PetscErrorCode monitorSolve(TS ts, PetscInt timestep, PetscReal time,
 	const int size = PetscSolver::getNetwork()->size();
 	// The array of cluster names
 	std::vector<std::string> names(size);
-	// Get the processor id
-	int procId;
-	MPI_Comm_rank(PETSC_COMM_WORLD, &procId);
-	// Header and output string streams
-	std::stringstream header, outputData;
-	// Create a stream for naming the file
-	std::stringstream outputFileNameStream;
-	outputFileNameStream << "xolotl_out_" << procId << "_" << timestep;
 	PetscErrorCode ierr;
-	PetscViewer viewer;
 	PetscReal *solutionArray, *gridPointSolution, x, hx;
 	PetscInt xs, xm, Mx;
 	int xi, i;
 
 	PetscFunctionBeginUser;
 
-	// Create the PETScViewer and get the data
+	// Get the data
 	VecGetArray(solution, &solutionArray);
-	PetscViewerASCIIOpen(PETSC_COMM_WORLD, outputFileNameStream.str().c_str(),
-			&viewer);
-
-	// Create the header for the file
-	auto reactants = PetscSolver::getNetwork()->getAll();
-	std::shared_ptr<PSICluster> cluster;
-	header << "# t x ";
-	for (int i = 0; i < size; i++) {
-		// Get the cluster from the list, its id and composition
-		cluster = std::dynamic_pointer_cast<PSICluster>(reactants->at(i));
-		int id = cluster->getId() - 1;
-		auto composition = cluster->getComposition();
-		// Make the header entry
-		std::stringstream name;
-		name << (cluster->getName()).c_str() << "_(" << composition["He"] << ","
-				<< composition["V"] << "," << composition["I"] << ") ";
-		// Push the header entry on the array
-		name >> names[id];
-	}
-	for (int i = 0; i < size; i++) {
-		header << names[i] << " ";
-	}
-	header << "\n";
-	PetscViewerASCIIPrintf(viewer, header.str().c_str());
 
 	// Get the da from ts
 	DM da;
@@ -87,6 +54,7 @@ static PetscErrorCode monitorSolve(TS ts, PetscInt timestep, PetscReal time,
 
 	// Get the corners of the grid
 	ierr = DMDAGetCorners(da, &xs, NULL, NULL, &xm, NULL, NULL);
+	checkPetscError(ierr);
 	ierr = DMDAGetInfo(da, PETSC_IGNORE, &Mx, PETSC_IGNORE, PETSC_IGNORE,
 			PETSC_IGNORE, PETSC_IGNORE, PETSC_IGNORE, PETSC_IGNORE,
 			PETSC_IGNORE, PETSC_IGNORE, PETSC_IGNORE, PETSC_IGNORE,
@@ -94,7 +62,6 @@ static PetscErrorCode monitorSolve(TS ts, PetscInt timestep, PetscReal time,
 	checkPetscError(ierr);
 	// Setup some step size variables
 	hx = 8.0 / (PetscReal) (Mx - 1);
-	checkPetscError(ierr);
 
 	// Create a Point vector to store the data to give to the data provider
 	// for the visualization
@@ -105,7 +72,6 @@ static PetscErrorCode monitorSolve(TS ts, PetscInt timestep, PetscReal time,
 	for (xi = xs; xi < xs + xm; xi++) {
 		// Dump x
 		x = xi * hx;
-		outputData << timestep << " " << x << " ";
 		// Get the pointer to the beginning of the solution data for this grid point
 		gridPointSolution = solutionArray + size * xi;
 		// Update the concentrations in the network to have physics results
@@ -115,19 +81,12 @@ static PetscErrorCode monitorSolve(TS ts, PetscInt timestep, PetscReal time,
 		double concentrations[size];
 		double * concentration = &concentrations[0];
 		PetscSolver::getNetwork()->fillConcentrationsArray(concentration);
-		// Dump the data to the stream
-		for (i = 0; i < size; i++) {
-			outputData << concentration[i] << " ";
-		}
 
-		// Create a Point and add it to myPoints
+		// Create a Point with the concentration[2] as the value and add it to myPoints
 		xolotlViz::Point aPoint;
 		aPoint.value = concentration[2];
 		aPoint.t = time; aPoint.x = x;
 		myPoints->push_back(aPoint);
-
-		// End the line
-		outputData << "\n";
 	}
 
 	// Get the data provider
@@ -144,12 +103,8 @@ static PetscErrorCode monitorSolve(TS ts, PetscInt timestep, PetscReal time,
 
 	// Render
 	plot->render();
-
-	// Dump the data to file
-	PetscViewerASCIIPrintf(viewer, outputData.str().c_str());
-	// Restore the array and kill the viewer
+	// Restore the array
 	VecRestoreArray(solution, &solutionArray);
-	PetscViewerDestroy(&viewer);
 
 	PetscFunctionReturn(0);
 }
@@ -194,3 +149,7 @@ PetscErrorCode setupPetscMonitor(TS ts) {
 	checkPetscError(ierr);
 	PetscFunctionReturn(0);
 }
+
+}
+
+/* end namespace xolotlSolver */
