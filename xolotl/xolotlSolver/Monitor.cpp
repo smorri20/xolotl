a50fe8f486be391985b7289ff771e78fe720dbd7
Sophie Blondel <sophie.blondel1@gmail.com>
2014-04-15 18:30:39 +0000

    Example of Monitor plotting performances: the current timer of the solver for each process ID. SB 20140415
    git-svn-id: file:///Users/MacAir/Dropbox/xolotl-cleaned/branches/visualization_implementation_branch@363 72105f29-0fe1-43bd-9363-b1e281e962a2

--- xolotl/xolotlSolver/Monitor.cpp
+++ xolotl/xolotlSolver/Monitor.cpp
@@ -1,8 +1,8 @@
 // Includes
 #include "../xolotlViz/plot/Plot.h"
 #include "../xolotlViz/dataprovider/Point.h"
-#include "../xolotlViz/plot/SurfacePlot.h"
-#include "../xolotlViz/dataprovider/CvsXYDataProvider.h"
+#include "../xolotlViz/plot/ScatterPlot.h"
+#include "../xolotlViz/dataprovider/CvsXDataProvider.h"
 #include "../xolotlViz/labelprovider/LabelProvider.h"
 #include "PetscSolver.h"
 #include <petscts.h>
@@ -34,114 +34,79 @@ std::shared_ptr<xolotlViz::Plot> plot;
  */
 static PetscErrorCode monitorSolve(TS ts, PetscInt timestep, PetscReal time,
 		Vec solution, void *ictx) {
-	// Network size
-	const int size = PetscSolver::getNetwork()->size();
-	// The array of cluster names
-	std::vector<std::string> names(size);
-	PetscErrorCode ierr;
-	PetscReal *solutionArray, *gridPointSolution, x, hx;
-	Vec localSolution;
-	PetscInt xs, xm, Mx;
-	int xi, i;
 
 	PetscFunctionBeginUser;
 
-	int procId;
-	MPI_Comm_rank(MPI_COMM_WORLD, &procId);
+	// Get the number of processes
+	int size;
+	MPI_Comm_size(PETSC_COMM_WORLD, &size);
 
-	// Fill the array of clusters name because the Id is not the same as
-	// reactants->at(i)
-	auto reactants = PetscSolver::getNetwork()->getAll();
-	std::shared_ptr<PSICluster> cluster;
-	for (int i = 0; i < size; i++) {
-
-		// Get the cluster from the list, its id and composition
-		cluster = std::dynamic_pointer_cast<PSICluster>(reactants->at(i));
-		int id = cluster->getId() - 1;
-		auto composition = cluster->getComposition();
-
-		// Create the name
-		std::stringstream name;
-		name << (cluster->getName()).c_str() << "(" << composition["He"] << ","
-				<< composition["V"] << "," << composition["I"] << ") ";
-
-		// Push the header entry on the array
-		name >> names[id];
+	// Print a warning if only one process
+	if (size == 1) {
+		cout << "You are trying to plot things that don't have any sense!! "
+				<< "\nRemove -mymonitor or run in parallel." << endl;
+		PetscFunctionReturn(0);
 	}
 
-	// Get the data
-	VecGetArray(solution, &solutionArray);
+	// Get the current process ID
+	int procId;
+	MPI_Comm_rank(PETSC_COMM_WORLD, &procId);
 
-	// Get the da from ts
-	DM da;
-	ierr = TSGetDM(ts, &da);
-	checkPetscError(ierr);
+	// Get the solve timer
+	auto solverTimer = xolotlPerf::getHandlerRegistry()->getTimer("solve");
 
-	// Get the local vector, which is capital when running in parallel,
-	// and put it into solutionArray
-	ierr = DMGetLocalVector(da, &localSolution);
-	checkPetscError(ierr);
-	ierr = DMGlobalToLocalBegin(da, solution, INSERT_VALUES, localSolution);
-	checkPetscError(ierr);
-	ierr = DMGlobalToLocalEnd(da, solution, INSERT_VALUES, localSolution);
-	checkPetscError(ierr);
-	ierr = DMDAVecGetArray(da, localSolution, &solutionArray);
-	checkPetscError(ierr);
+	// Stop it to access its value
+	solverTimer->stop();
 
-	// Get the corners of the grid
-	ierr = DMDAGetCorners(da, &xs, NULL, NULL, &xm, NULL, NULL);
-	checkPetscError(ierr);
-	ierr = DMDAGetInfo(da, PETSC_IGNORE, &Mx, PETSC_IGNORE, PETSC_IGNORE,
-			PETSC_IGNORE, PETSC_IGNORE, PETSC_IGNORE, PETSC_IGNORE,
-			PETSC_IGNORE, PETSC_IGNORE, PETSC_IGNORE, PETSC_IGNORE,
-			PETSC_IGNORE);
-	checkPetscError(ierr);
-	// Setup some step size variables
-	hx = 8.0 / (PetscReal) (Mx - 1);
+	// Master process
+	if (procId == 0) {
 
-	// Create a Point vector to store the data to give to the data provider
-	// for the visualization
-	auto myPoints = std::make_shared< std::vector<xolotlViz::Point> >();
+		// Create a Point vector to store the data to give to the data provider
+		// for the visualization
+		auto myPoints = std::make_shared<std::vector<xolotlViz::Point> >();
 
-	// Choice of the cluster to be plotted
-	int iCluster = 2;
+		// Give it the value for procId = 0
+		xolotlViz::Point aPoint;
+		aPoint.value = solverTimer->getValue();
+		aPoint.t = time;
+		aPoint.x = procId;
+		myPoints->push_back(aPoint);
 
-	// Print the solution data
-	for (xi = xs; xi < xs + xm; xi++) {
-		// Dump x
-		x = xi * hx;
-		// Get the pointer to the beginning of the solution data for this grid point
-		gridPointSolution = solutionArray + size * xi;
-		// Update the concentrations in the network to have physics results
-		// (non negative)
-		PetscSolver::getNetwork()->updateConcentrationsFromArray(gridPointSolution);
-		// Get the concentrations from the network
-		double concentrations[size];
-		double * concentration = &concentrations[0];
-		PetscSolver::getNetwork()->fillConcentrationsArray(concentration);
+		// Loop on all the other processes
+		for (int i = 1; i < size; i++) {
+			double counter = 0.;
 
-		for (int i = 0; i < (int) xm; i++) {
-			// Create a Point with the concentration[iCluster] as the value
-			// and add it to myPoints
-			xolotlViz::Point aPoint;
-			aPoint.value = concentration[2];
-			aPoint.t = time; aPoint.x = x; aPoint.y = (double) i;
+			// Receive the value from the other processes
+			MPI_Recv(&counter, 1, MPI_DOUBLE, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+			// Give it the value for procId = i
+			aPoint.value = counter;
+			aPoint.t = time;
+			aPoint.x = i;
 			myPoints->push_back(aPoint);
 		}
+
+		// Get the data provider and give it the points
+		plot->getDataProvider()->setPoints(myPoints);
+
+		// Change the title of the plot
+		std::stringstream title;
+		title << "timeTS" << timestep << ".pnm";
+		plot->plotLabelProvider->titleLabel = title.str();
+
+		// Render
+		plot->render();
 	}
 
-	// Get the data provider and give it the points
-	plot->getDataProvider()->setPoints(myPoints);
+	else {
+		double counter = solverTimer->getValue();
 
-	// Change the title of the plot
-	std::stringstream title;
-	title << "blaTS" << timestep << "_" << procId << ".pnm";
-	plot->plotLabelProvider->titleLabel = title.str();
+		// Send the value of the timer to the master process
+		MPI_Send(&counter, 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
+	}
 
-	// Render
-	plot->render();
-	// Restore the array
-	VecRestoreArray(solution, &solutionArray);
+	// Restart the timer
+	solverTimer->start();
 
 	PetscFunctionReturn(0);
 }
@@ -162,19 +127,18 @@ PetscErrorCode setupPetscMonitor(TS ts) {
 		PetscFunctionReturn(0);
 
 	// Create a ScatterPlot
-	plot = std::make_shared<xolotlViz::SurfacePlot> ();
+	plot = std::make_shared<xolotlViz::ScatterPlot>();
 
 	// Create and set the label provider
 	auto labelProvider = std::make_shared<xolotlViz::LabelProvider>();
 	labelProvider->axis1Label = "x Position on the Grid";
-	labelProvider->axis2Label = "y Position on the Grid";
-	labelProvider->axis3Label = "Concentration";
+	labelProvider->axis2Label = "Concentration";
 
 	// Give it to the plot
 	plot->setLabelProvider(labelProvider);
 
 	// Create the data provider
-	auto dataProvider = std::make_shared<xolotlViz::CvsXYDataProvider>();
+	auto dataProvider = std::make_shared<xolotlViz::CvsXDataProvider>();
 
 	// Give it to the plot
 	plot->setDataProvider(dataProvider);
