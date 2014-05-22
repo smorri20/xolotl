9644e54adcffa939905f103b45f9f677e8522944
jayjaybillings <jayjaybillings@mycompany.com>
2014-05-22 19:01:19 +0000

    Made several design enhancements to cache temperature-dependent values and improve network access. ~JJB 20140522 15:02
    git-svn-id: file:///Users/MacAir/Dropbox/xolotl-cleaned/branches/performance_optimization@439 72105f29-0fe1-43bd-9363-b1e281e962a2

---
+++ xolotl/xolotlCore/reactants/Reactant.cpp
@@ -0,0 +1,127 @@
+// Includes
+#include "Reactant.h"
+#include "ReactionNetwork.h"
+#include "xolotlPerf/HandlerRegistryFactory.h"
+#include <iostream>
+
+// Namespaces
+using namespace xolotlCore;
+
+Reactant::Reactant() :
+		concentration(0.0), name(""), id(0), temperature(0.0) {
+
+	// Temporary definition used to specify the type of performance handler registry
+	// that will be used (standard or dummy).  Currently, the handler registry is set via
+	// commandline argument and the only way to access which handler registry will be used
+	// is through getHandlerRegistry
+//	handlerRegistry = xolotlPerf::getHandlerRegistry();
+}
+
+Reactant::Reactant(std::shared_ptr<xolotlPerf::IHandlerRegistry> registry) :
+		concentration(0.0), name(""), id(0), temperature(0.0), handlerRegistry(registry) {
+
+}
+
+Reactant::Reactant(const Reactant &other) :
+		concentration(other.concentration), name(other.name), id(other.id),
+		compositionMap(other.compositionMap), temperature(0.0), handlerRegistry(other.handlerRegistry), typeName(other.typeName)  {
+}
+
+Reactant::~Reactant() {
+}
+
+std::shared_ptr<Reactant> Reactant::clone() {
+	std::shared_ptr<Reactant> reactant(new Reactant(*this));
+	return reactant;
+}
+
+Reactant::Reactant(double conc, std::shared_ptr<xolotlPerf::IHandlerRegistry> registry) :
+		concentration(conc), name(""), id(0), handlerRegistry(registry) {
+}
+
+double Reactant::getConcentration() const {
+	return concentration;
+}
+
+void Reactant::increaseConcentration(double deltaConc) {
+	concentration += deltaConc;
+	std::cout << "Conc = " << concentration << ", delta = " << deltaConc << std::endl;
+	return;
+}
+
+void Reactant::decreaseConcentration(double deltaConc) {
+	concentration -= deltaConc;
+	return;
+}
+
+void Reactant::setConcentration(double conc) {
+	concentration = std::max(conc, 0.);
+	return;
+}
+
+void Reactant::zero() {
+	concentration = 0.0;
+	return;
+}
+
+double Reactant::getTotalFlux(const double temperature) {
+	return 0.0;
+}
+
+void Reactant::setReactionNetwork(
+	std::shared_ptr<ReactionNetwork> reactionNetwork) {
+	
+	network = reactionNetwork;
+}
+
+
+std::vector<int> Reactant::getConnectivity() const {
+	// The connectivity array by default is filled with
+	// zeroes.
+	int connectivityLength = network->size();
+	std::vector<int> connectivity = std::vector<int>(connectivityLength, 0);
+
+	return connectivity;
+}
+
+
+/**
+ * This operation returns the list of partial derivatives of this Reactant
+ * with respect to all other reactants in the network. The combined lists
+ * of partial derivatives from all of the reactants in the network can be
+ * used to form, for example, a Jacobian.
+ *
+ * @param the temperature at which the reactions are occurring
+ * @return The partial derivatives for this reactant where index zero
+ * corresponds to the first reactant in the list returned by the
+ * ReactionNetwork::getAll() operation.
+ */
+std::vector<double> Reactant::getPartialDerivatives(double temperature) const {
+	// The partial derivatives array by default is filled with
+	// zeroes.
+	int length = network->size();
+	std::vector<double> partial_derivatives = std::vector<double>(length, 0.0);
+
+	return partial_derivatives;
+}
+
+/**
+ * This operation returns the name of the reactant.
+ * @return the name
+ */
+const std::string Reactant::getName() const {
+	return name;
+}
+
+std::string Reactant::getType() const {
+	return typeName;
+}
+
+const std::string Reactant::toString() {
+	throw std::string("Reactant::toString() is unimplemented");
+}
+
+const std::map<std::string, int> & Reactant::getComposition() const {
+	// Return the composition map
+	return compositionMap;
+}
---
+++ xolotl/xolotlCore/reactants/psiclusters/InterstitialCluster.cpp
@@ -0,0 +1,149 @@
+#include "InterstitialCluster.h"
+#include <Constants.h>
+#include <iostream>
+
+using namespace xolotlCore;
+
+InterstitialCluster::InterstitialCluster(int nI, std::shared_ptr<xolotlPerf::IHandlerRegistry> registry) :
+		PSICluster(nI, registry) {
+
+	// Update the composition map
+	compositionMap["I"] = size;
+
+	// Set the reactant name appropriately
+	std::stringstream nameStream;
+	nameStream << "I_" << size;
+	name = nameStream.str();
+	// Set the typename appropriately
+	typeName = "I";
+
+	// Compute the reaction radius
+	double EightPi = 8.0 * xolotlCore::pi;
+	double aCubed = pow(xolotlCore::latticeConstant, 3.0);
+	double termOne = 1.15 * (sqrt(3.0) / 4.0) * xolotlCore::latticeConstant;
+	double termTwo = pow((3.0 / EightPi) * aCubed * size, (1.0 / 3.0));
+	double termThree = pow((3.0 / EightPi) * aCubed, (1.0 / 3.0));
+	reactionRadius = termOne + termTwo - termThree;
+
+}
+
+InterstitialCluster::~InterstitialCluster() {
+}
+
+std::shared_ptr<Reactant> InterstitialCluster::clone() {
+	std::shared_ptr<Reactant> reactant(new InterstitialCluster(*this));
+	return reactant;
+}
+
+void InterstitialCluster::createReactionConnectivity() {
+
+	// Local Declarations - Note the reference to the properties map
+	auto props = network->getProperties();
+	int maxIClusterSize = std::stoi(props["maxIClusterSize"]);
+	int maxHeIClusterSize = std::stoi(props["maxHeIClusterSize"]);
+	int numHeVClusters = std::stoi(props["numHeVClusters"]);
+	int numHeIClusters = std::stoi(props["numHeIClusters"]);
+	int firstSize = 0, secondSize = 0, reactantsSize = 0;
+	std::shared_ptr<PSICluster> firstReactant, secondReactant;
+
+	// Connect this cluster to itself since any reaction will affect it
+	setReactionConnectivity(getId());
+
+	/*
+	 * This section fills the array of reacting pairs that combine to produce
+	 * this cluster from I clusters that are smaller than this.size. Each
+	 * cluster i combines with a second cluster of size this.size - i.size.
+	 *
+	 * Total size starts with a value of one so that clusters of size one are
+	 * not considered in this loop.
+	 */
+	for (firstSize = 1; firstSize <= (int) size/2; firstSize++) {
+		secondSize = size - firstSize;
+		// Get the first and second reactants for the reaction
+		// first + second = this.
+		firstReactant = std::dynamic_pointer_cast<PSICluster>(network->get("I", firstSize));
+		secondReactant = std::dynamic_pointer_cast<PSICluster>(network->get("I", secondSize));
+		// Create a ReactingPair with the two reactants
+		if (firstReactant && secondReactant) {
+			ReactingPair pair;
+			pair.first = firstReactant;
+			pair.second = secondReactant;
+			// Add the pair to the list
+			reactingPairs.push_back(pair);
+		}
+	}
+
+	/* ----- I_a + I_b --> I_(a+b) -----
+	 *	Interstitial absorption
+	 *
+	 * All of these clusters are added to the set of combining reactants
+	 * because they contribute to the flux due to combination reactions.
+	 */
+	auto reactants = network->getAll("I");
+	combineClusters(reactants,maxIClusterSize,"I");
+
+	/* ----- He_a + I_b --> (He_a)*(I_b)
+	 * Interstitials can interact with clusters of He to form HeI clusters.
+	 * They cannot cluster with He clusters that are so large that the
+	 * combination of the two would produce an HeI cluster above the
+	 * maximum size.
+	 *
+	 * All of these clusters are added to the set of combining reactants
+	 * because they contribute to the flux due to combination reactions.
+	 */
+	reactants = network->getAll("He");
+	combineClusters(reactants,maxHeIClusterSize,"HeI");
+
+	/* ----- I_a + V_b -----
+	 * --> I_(a-b), if a > b
+	 * --> V_(b-a), if a < b
+	 * --> 0, if a = b
+	 * Interstitial-Vacancy Annihilation
+	 *
+	 * All of these clusters are added to the set of combining reactants
+	 * because they contribute to the flux due to combination reactions.
+	 */
+	reactants = network->getAll("V");
+	fillVWithI("V",reactants);
+	// Mark the reaction connectivity for the cases where this cluster is
+	// produced by the above reaction. This has to be checked for every
+	// vacancy.
+	reactantsSize = reactants->size();
+	for (int i = 0; i < reactantsSize; i++) {
+		firstReactant = std::dynamic_pointer_cast<PSICluster>(reactants->at(i));
+		// Get the interstitial cluster that is bigger than the vacancy
+		// and can form this cluster. I only results when it is bigger than V.
+		secondReactant = std::dynamic_pointer_cast<PSICluster>(network->get("I",firstReactant->getSize() + size));
+		// Update the connectivity
+		if (secondReactant) {
+			setReactionConnectivity(firstReactant->getId());
+			setReactionConnectivity(secondReactant->getId());
+		}
+	}
+
+	/* ----- (He_a)(V_b) + (I_c) --> (He_a)[V_(b-c)] -----
+	 * Interstitials interact with all mixed-species clusters by
+	 * annihilating vacancies.
+	 *
+	 * All of these clusters are added to the set of combining reactants
+	 * because they contribute to the flux due to combination reactions.
+	 */
+	if (numHeVClusters > 0) {
+		reactants = network->getAll("HeV");
+		replaceInCompound(reactants,"V","I");
+	}
+
+	/* ----- (He_a)*(I_b) + I --> (He_a)*[I_(b + 1)] -----
+	 * Single interstitial absorption by a HeI cluster under the condition
+	 * that (x + y + 1) <= maxSize
+	 *
+	 * All of these clusters are added to the set of combining reactants
+	 * because they contribute to the flux due to combination reactions.
+	 */
+	if (size == 1 && numHeIClusters > 0) {
+		reactants = network->getAll("HeI");
+		combineClusters(reactants,maxHeIClusterSize,"HeI");
+	}
+
+	return;
+}
