e7e67bb78f20f966cb21383f63cb5394ab24c69e
cjernigan <cjernigan@mycompany.com>
2014-06-18 15:43:56 +0000

    Modified ArgumentsTest to use try/catch blocks.  CLJ 20140618
    git-svn-id: file:///Users/MacAir/Dropbox/xolotl-cleaned/branches/preprocessor_branch@513 72105f29-0fe1-43bd-9363-b1e281e962a2

--- xolotl/gov.ornl.xolotl.preprocessor.test/src/gov/ornl/xolotl/preprocessor/test/ArgumentsTest.java
+++ xolotl/gov.ornl.xolotl.preprocessor.test/src/gov/ornl/xolotl/preprocessor/test/ArgumentsTest.java
@@ -15,44 +15,52 @@ import uk.co.flamingpenguin.jewel.cli.CliFactory;
 public class ArgumentsTest {
 
 	/**
-	 * This operation checks the default Arguments.
+	 * This operation checks that the default argument values are
+	 * used if no command line options are specified.
 	 */
 	@Test
-	public void testDefaultArguments() throws ArgumentValidationException {
+	public void testDefaultArguments() {
 
 		// Local Declarations
-		final Arguments args = CliFactory.parseArguments(Arguments.class, new String [] {});
+		final Arguments args;
 
-		// Check that the default material is W
-		assertEquals("W", args.getMaterial());
+		try {
+			// Parse the empty string of arguments
+			args = CliFactory.parseArguments(Arguments.class, new String [] {});
+			
+			// Check that the default material is W
+			assertEquals("W", args.getMaterial());
 
-		// Check that the default startTemp is 1000
-		assertEquals("1000", args.getStartTemp());
+			// Check that the default startTemp is 1000
+			assertEquals("1000", args.getStartTemp());
 
-		// Check if there is a default tempFile 
-		assertEquals(false, args.isTempFile());
+			// Check if there is a default tempFile 
+			assertEquals(false, args.isTempFile());
 
-		// Check that the default heFlux is 2.5e27
-		assertEquals("2.5e27", args.getHeFlux());
-		
-		// Check that the default perfHandler is dummy
-		assertEquals("dummy", args.getPerfHandler());
-		
-		// Check that the default vizHandler is dummy
-		assertEquals("dummy", args.getVizHandler());
-		
-		// Check that the default checkpoint is true
-		assertEquals("true", args.getCheckpoint());
-		
-		// Check that the default networkFile is test.h5
-		assertEquals("test.h5", args.getNetworkFile());
-		
-		// Check the default petscArgs
-		assertEquals("-da_grid_x 10 -ts_final_time 1000"
-				+ "-ts_max_steps 3 -ts_adapt_dt_max 10 -ts_max_snes_failures 200"
-				+ "-pc_type fieldsplit -pc_fieldsplit_detect_coupling -fieldsplit_0_pc_type redundant"
-				+ "-fieldsplit_1_pc_type sor -snes_monitor -ksp_monitor -ts_monitor",
-				args.getPetscArgs());
+			// Check that the default heFlux is 2.5e27
+			assertEquals("2.5e27", args.getHeFlux());
+			
+			// Check that the default perfHandler is dummy
+			assertEquals("dummy", args.getPerfHandler());
+			
+			// Check that the default vizHandler is dummy
+			assertEquals("dummy", args.getVizHandler());
+			
+			// Check that the default checkpoint is true
+			assertEquals("true", args.getCheckpoint());
+			
+			// Check that the default networkFile is test.h5
+			assertEquals("test.h5", args.getNetworkFile());
+			
+			// Check the default petscArgs
+			assertEquals("-da_grid_x 10 -ts_final_time 1000"
+					+ "-ts_max_steps 3 -ts_adapt_dt_max 10 -ts_max_snes_failures 200"
+					+ "-pc_type fieldsplit -pc_fieldsplit_detect_coupling -fieldsplit_0_pc_type redundant"
+					+ "-fieldsplit_1_pc_type sor -snes_monitor -ksp_monitor -ts_monitor",
+					args.getPetscArgs());
+		} catch (ArgumentValidationException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	/**
@@ -63,40 +71,47 @@ public class ArgumentsTest {
 	public void testSpecifiedArguments() throws ArgumentValidationException {
 
 		// Local Declarations
-		final Arguments args = CliFactory.parseArguments(Arguments.class, 
-				new String [] {"--startTemp", "900", "--material", "Fe",
-								"--perfHandler", "std"});
+		final Arguments args;
+		
+		try {
+			// Parse the specified string of arguments
+			args= CliFactory.parseArguments(Arguments.class, 
+					new String [] {"--startTemp", "900", "--material", "Fe",
+				"--perfHandler", "std"});
+			
+			// Check that the material is Fe
+			assertEquals("Fe", args.getMaterial());
 
-		// Check that the default material is W
-		assertEquals("Fe", args.getMaterial());
+			// Check that the startTemp is 900
+			assertEquals("900", args.getStartTemp());
 
-		// Check that the default startTemp is 1000
-		assertEquals("900", args.getStartTemp());
+			// Check if there is a default tempFile 
+			assertEquals(false, args.isTempFile());
 
-		// Check if there is a default tempFile 
-		assertEquals(false, args.isTempFile());
-
-		// Check that the default heFlux is 2.5e27
-		assertEquals("2.5e27", args.getHeFlux());
-		
-		// Check that the default perfHandler is dummy
-		assertEquals("std", args.getPerfHandler());
-		
-		// Check that the default vizHandler is dummy
-		assertEquals("dummy", args.getVizHandler());
-		
-		// Check that the default checkpoint is true
-		assertEquals("true", args.getCheckpoint());
-		
-		// Check that the default networkFile is test.h5
-		assertEquals("test.h5", args.getNetworkFile());
-		
-		// Check the default petscArgs
-		assertEquals("-da_grid_x 10 -ts_final_time 1000"
-				+ "-ts_max_steps 3 -ts_adapt_dt_max 10 -ts_max_snes_failures 200"
-				+ "-pc_type fieldsplit -pc_fieldsplit_detect_coupling -fieldsplit_0_pc_type redundant"
-				+ "-fieldsplit_1_pc_type sor -snes_monitor -ksp_monitor -ts_monitor",
-				args.getPetscArgs());
+			// Check that the default heFlux is 2.5e27
+			assertEquals("2.5e27", args.getHeFlux());
+			
+			// Check that the default perfHandler is dummy
+			assertEquals("std", args.getPerfHandler());
+			
+			// Check that the default vizHandler is dummy
+			assertEquals("dummy", args.getVizHandler());
+			
+			// Check that the default checkpoint is true
+			assertEquals("true", args.getCheckpoint());
+			
+			// Check that the default networkFile is test.h5
+			assertEquals("test.h5", args.getNetworkFile());
+			
+			// Check the default petscArgs
+			assertEquals("-da_grid_x 10 -ts_final_time 1000"
+					+ "-ts_max_steps 3 -ts_adapt_dt_max 10 -ts_max_snes_failures 200"
+					+ "-pc_type fieldsplit -pc_fieldsplit_detect_coupling -fieldsplit_0_pc_type redundant"
+					+ "-fieldsplit_1_pc_type sor -snes_monitor -ksp_monitor -ts_monitor",
+					args.getPetscArgs());
+		} catch (ArgumentValidationException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
