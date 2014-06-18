9652171190b0e05de76fc6dd73014db8e952aead
Sophie Blondel <sophie.blondel1@gmail.com>
2014-06-18 16:04:38 +0000

    Adding tests for HDF5 writing and generateGrid. SB 20140608
    git-svn-id: file:///Users/MacAir/Dropbox/xolotl-cleaned/branches/preprocessor_branch@514 72105f29-0fe1-43bd-9363-b1e281e962a2

--- xolotl/gov.ornl.xolotl.preprocessor.test/src/gov/ornl/xolotl/preprocessor/test/PreprocessorTest.java
+++ xolotl/gov.ornl.xolotl.preprocessor.test/src/gov/ornl/xolotl/preprocessor/test/PreprocessorTest.java
@@ -3,9 +3,11 @@ package gov.ornl.xolotl.preprocessor.test;
 import static org.junit.Assert.*;
 
 import java.util.Properties;
+import java.util.ArrayList;
 import java.io.*;
 
 import gov.ornl.xolotl.preprocessor.Preprocessor;
+import gov.ornl.xolotl.preprocessor.Cluster;
 
 import org.junit.Test;
 
@@ -22,7 +24,7 @@ public class PreprocessorTest {
 
 		// Local Declarations
 		Preprocessor preprocessor = new Preprocessor();
-		
+
 		// Generate the default parameters
 		Properties defaults = preprocessor.generateParameters();
 
@@ -37,32 +39,32 @@ public class PreprocessorTest {
 
 		// Check that the default heFlux is 2.5e27
 		assertEquals("2.5e27", defaults.getProperty("heFlux"));
-		
+
 		// Check that the default perfHandler is dummy
 		assertEquals("dummy", defaults.getProperty("perfHandler"));
-		
+
 		// Check that the default vizHandler is dummy
 		assertEquals("dummy", defaults.getProperty("vizHandler"));
-		
+
 		// Check that the default checkpoint is true
 		assertEquals("true", defaults.getProperty("checkpoint"));
-		
+
 		// Check that the default networkFile is test.h5
 		assertEquals("test.h5", defaults.getProperty("networkFile"));
-		
+
 		// Check the default petscArgs
-		assertEquals("-ts_final_time 1000 -ts_adapt_dt_max 10 "
-				+ "-ts_max_snes_failures 200 -pc_type fieldsplit -pc_fieldsplit_detect_coupling "
-				+ "-fieldsplit_0_pc_type redundant -fieldsplit_1_pc_type sor -snes_monitor "
-				+ "-ksp_monitor -da_grid_x 10 -ts_max_steps 3 -ts_monitor",
+		assertEquals(
+				"-ts_final_time 1000 -ts_adapt_dt_max 10 "
+						+ "-ts_max_snes_failures 200 -pc_type fieldsplit -pc_fieldsplit_detect_coupling "
+						+ "-fieldsplit_0_pc_type redundant -fieldsplit_1_pc_type sor -snes_monitor "
+						+ "-ksp_monitor -da_grid_x 10 -ts_max_steps 3 -ts_monitor",
 				defaults.getProperty("petscArgs"));
 
 		return;
 	}
 
 	/**
-	 * This operation makes sure that the parameters can be written
-	 * to a file.
+	 * This operation makes sure that the parameters can be written to a file.
 	 */
 	@Test
 	public void testWriteParameterFile() {
@@ -70,32 +72,120 @@ public class PreprocessorTest {
 		// Local Declarations
 		Preprocessor preprocessor = new Preprocessor();
 		InputStream inParamsFile = null;
-		
+
 		// Generate the default parameters
 		Properties defaults = preprocessor.generateParameters();
 		// Write the parameter file
 		preprocessor.writeParameterFile("paramsTest", defaults);
-		
-		// Create a new Properties object in order to check that 
-		// the correct parameters were written to the paramsTest file 
+
+		// Create a new Properties object in order to check that
+		// the correct parameters were written to the paramsTest file
 		Properties params = new Properties();
-		
+
 		try {
 
 			inParamsFile = new FileInputStream("paramsTest");
 			// Load the properties file
 			params.load(inParamsFile);
-			
+
 			// Check that the parameters from the input file are
 			// the default parameters
 			assertEquals("W", params.getProperty("material"));
 			assertEquals("2.5e27", params.getProperty("heFlux"));
 			assertEquals("dummy", params.getProperty("perfHandler"));
 			assertEquals("1000", params.getProperty("startTemp"));
-			
+
 			inParamsFile.close();
 
 		} catch (IOException e) {
+			// Complain and fail
+			e.printStackTrace();
+			fail();
+		}
+
+		return;
+	}
+
+	/**
+	 * This operation checks generateGrid.
+	 */
+	@Test
+	public void testGenerateGrid() {
+		// Local Declarations
+		Preprocessor preprocessor = new Preprocessor();
+
+		// Create the known grid array
+		double[] knownGrid = new double[15];
+
+		// Fill it with known values
+		for (int i = 0; i < 15; i++) {
+			knownGrid[i] = (double) i * 0.57142857142;
+		}
+
+		try {
+			// Generate a grid
+			double[] newGrid = preprocessor.generateGrid(8, 1);
+
+			// Check the length of it
+			assertEquals(newGrid.length, knownGrid.length);
+
+			// Check all the values
+			for (int i = 0; i < newGrid.length; i++) {
+				assertEquals(newGrid[i], knownGrid[i], 1.0e-5);
+			}
+
+		} catch (Exception e) {
+			// Complain and fail
+			e.printStackTrace();
+			fail();
+		}
+
+		return;
+	}
+
+	/**
+	 * This operation checks the writing of the HDF5 file.
+	 */
+	@Test
+	public void testHDF5Writing() {
+		// Local Declarations
+		Preprocessor preprocessor = new Preprocessor();
+
+		// Create an empty cluster array
+		ArrayList<Cluster> clusters = new ArrayList<Cluster>();
+
+		// Create a cluster
+		Cluster cluster = new Cluster();
+		cluster.nHe = 1;
+		cluster.nV = 23;
+		cluster.nI = 52;
+		cluster.E_He = 9.325;
+		cluster.E_V = 34.2346;
+		cluster.E_I = 3326424.2323543;
+		cluster.E_m = 0.04;
+		cluster.D_0 = 1.1;
+
+		// Add it to clusters
+		clusters.add(cluster);
+
+		try {
+			// Create the HDF5 file
+			preprocessor.createHDF5("test.h5");
+
+			// Write the header in it
+			int[] dim = { 4 };
+			int[] refinement = { 0 };
+			preprocessor.writeHeader("test.h5", dim, refinement);
+
+			// Write the network in it
+			preprocessor.writeNetwork("test.h5", clusters);
+
+			// Check that the file was created
+			File f = new File("test.h5");
+			boolean fileExists = (f.exists() && !f.isDirectory());
+			assertEquals(fileExists, true);
+
+		} catch (Exception e) {
 			// Complain and fail
 			e.printStackTrace();
 			fail();
