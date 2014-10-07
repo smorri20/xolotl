ea860255ae8df00bdcd77e720e998553c60d1096
Sophie Blondel <sophie.blondel1@gmail.com>
2014-10-07 20:13:56 +0000

    Creating a new branch to start working on the implementation of the advection term. SB 20141007
    git-svn-id: file:///Users/MacAir/Dropbox/xolotl-cleaned/branches/advection_branch@709 72105f29-0fe1-43bd-9363-b1e281e962a2

---
+++ xolotl/gov.ornl.xolotl.preprocessor/src/gov/ornl/xolotl/preprocessor/Main.java
@@ -0,0 +1,99 @@
+/**
+ * 
+ */
+package gov.ornl.xolotl.preprocessor;
+
+import java.util.ArrayList;
+
+import uk.co.flamingpenguin.jewel.cli.ArgumentValidationException;
+import uk.co.flamingpenguin.jewel.cli.CliFactory;
+
+/**
+ * This class launches the Xolotl preprocessor.
+ * 
+ * @author Jay Jay Billings
+ * 
+ */
+public class Main {
+
+	/**
+	 * This operation launches the preprocessor and creates the initial
+	 * conditions for Xolotl.
+	 * 
+	 * @param args
+	 *            Command line arguments.
+	 */
+	public static void main(String[] args) {
+		// Local Declarations
+		Arguments myArgs = null;
+
+		// Print some information to let the user know what is happening.
+		System.out.println("Starting Xolotl Preprocessor...");
+
+		// Get command line arguments
+		try {
+			// Parse the command line arguments.
+			myArgs = CliFactory.parseArguments(Arguments.class, args);
+			System.out.println("Command line arguments loaded.");
+
+			// Generate the input if the arguments are valid.
+			if (myArgs != null) {
+				// Create the Preprocessor
+				Preprocessor preprocessor = new Preprocessor(myArgs);
+
+				// Generate the network of clusters
+				ArrayList<Cluster> clusters = preprocessor
+						.generateNetwork(args);
+				System.out.println("Network generated.");
+				
+				// Get the name of the networkFile from xolotlParams
+				String networkFileName = preprocessor.xolotlParams.getProperty("networkFile");
+
+				// Create the HDF5 file
+				preprocessor.createHDF5(networkFileName);
+
+				// Write the network in it
+				preprocessor.writeNetwork(networkFileName, clusters);
+
+				if (myArgs.isCheckpoint()) {
+					String HDF5FileName = myArgs.getCheckpoint();
+					// Read the header and the concentration from this file 
+					// and copy them to the network file
+					int gridPoints = preprocessor.copyHeader(HDF5FileName, networkFileName);
+					preprocessor.copyConcentration(HDF5FileName, networkFileName, gridPoints, clusters);
+				}
+				else {
+					// Get the grid size
+					String xgrid = preprocessor.petscOptions.get("-da_grid_x");
+					
+					// Write the header in it
+					int[] dim = { Integer.parseInt(xgrid) };
+					int[] refinement = { 0 };
+					preprocessor.writeHeader(networkFileName, dim, refinement);
+				}
+
+				System.out.println("HDF5 file generated.");
+
+				// Write the file containing the parameters that are needed
+				// to run Xolotl
+				preprocessor.writeParameterFile("params.txt",
+						preprocessor.xolotlParams);
+				System.out.println("Parameters written.");
+			}
+		} catch (ArgumentValidationException e1) {
+			System.err.println("Unable to print input arguments. Aborting.");
+			e1.printStackTrace();
+			return;
+		} catch (Exception e) {
+			System.err.println("Exception caught while generating input. "
+					+ "Aborting.");
+			e.printStackTrace();
+			return;
+		}
+
+		// Say goodbye
+		System.out.println("Finished.");
+
+		return;
+	}
+}
---
+++ xolotl/main.cpp
@@ -0,0 +1,240 @@
+/**
+ * Main.c, currently only able to load clusters
+ */
+#include <cstdlib>
+#include <iostream>
+#include <fstream>
+#include <cassert>
+#include <Reactant.h>
+#include <PSIClusterNetworkLoader.h>
+#include <PetscSolver.h>
+#include <mpi.h>
+#include <MPIUtils.h>
+#include <Options.h>
+#include <xolotlPerf.h>
+#include <MaterialHandlerFactory.h>
+#include <TemperatureHandlerFactory.h>
+#include <VizHandlerRegistryFactory.h>
+#include <HDF5NetworkLoader.h>
+#include <IVizHandlerRegistry.h>
+#include <ctime>
+
+using namespace std;
+using std::shared_ptr;
+namespace xperf = xolotlPerf;
+
+
+//! This operation prints the start message
+void printStartMessage() {
+	std::cout << "Starting Xolotl Plasma-Surface Interactions Simulator" << std::endl;
+	// TODO! Print copyright message
+	// Print date and time
+	std::time_t currentTime = std::time(NULL);
+	std::cout << std::asctime(std::localtime(&currentTime)); // << std::endl;
+}
+
+bool initMaterial(Options &options) {
+
+	bool materialInitOK = xolotlSolver::initializeMaterial(options);
+	if (!materialInitOK) {
+		std::cerr << "Unable to initialize requested material.  Aborting"
+				<< std::endl;
+		return EXIT_FAILURE;
+	} else
+		return materialInitOK;
+}
+
+bool initTemp(Options &options) {
+
+	bool tempInitOK = xolotlSolver::initializeTempHandler(options);
+	if (!tempInitOK) {
+		std::cerr << "Unable to initialize requested temperature.  Aborting"
+				<< std::endl;
+		return EXIT_FAILURE;
+	} else
+		return tempInitOK;
+}
+
+
+bool initViz(bool opts) {
+
+	bool vizInitOK = xolotlViz::initialize(opts);
+	if (!vizInitOK) {
+		std::cerr
+				<< "Unable to initialize requested visualization infrastructure. "
+				<< "Aborting" << std::endl;
+		return EXIT_FAILURE;
+	} else
+		return vizInitOK;
+}
+
+std::shared_ptr<xolotlSolver::PetscSolver> setUpSolver(
+		std::shared_ptr<xolotlPerf::IHandlerRegistry> handlerRegistry, int argc,
+		char **argv) {
+	// Setup the solver
+	auto solverInitTimer = handlerRegistry->getTimer("initSolver");
+	solverInitTimer->start();
+	std::shared_ptr<xolotlSolver::PetscSolver> solver = std::make_shared<
+			xolotlSolver::PetscSolver>(handlerRegistry);
+	solver->setCommandLineOptions(argc, argv);
+	solver->initialize();
+	solverInitTimer->stop();
+
+	return solver;
+}
+
+void launchPetscSolver(std::shared_ptr<xolotlSolver::PetscSolver> solver,
+		std::shared_ptr<xolotlPerf::IHandlerRegistry> handlerRegistry,
+		std::shared_ptr<xolotlSolver::IFluxHandler> materialHandler,
+		std::shared_ptr<xolotlSolver::ITemperatureHandler> tempHandler,
+		double stepSize) {
+
+    xperf::IHardwareCounter::SpecType hwctrSpec;
+    hwctrSpec.push_back( xperf::IHardwareCounter::FPOps );
+    hwctrSpec.push_back( xperf::IHardwareCounter::Cycles );
+    hwctrSpec.push_back( xperf::IHardwareCounter::L3CacheMisses );
+
+	// Launch the PetscSolver
+	auto solverTimer = handlerRegistry->getTimer("solve");
+    auto solverHwctr = handlerRegistry->getHardwareCounter( "solve", hwctrSpec );
+	solverTimer->start();
+    solverHwctr->start();
+	solver->solve(materialHandler, tempHandler, stepSize);
+    solverHwctr->stop();
+	solverTimer->stop();
+}
+
+std::shared_ptr<PSIClusterNetworkLoader> setUpNetworkLoader(int rank,
+		MPI_Comm comm, std::string networkFilename,
+		std::shared_ptr<xolotlPerf::IHandlerRegistry> registry) {
+
+	// Create a HDF5NetworkLoader
+	std::shared_ptr<HDF5NetworkLoader> networkLoader;
+	networkLoader = std::make_shared<HDF5NetworkLoader>(registry);
+	// Give the networkFilename to the network loader
+	networkLoader->setFilename(networkFilename);
+
+	return networkLoader;
+}
+
+
+//! Main program
+int main(int argc, char **argv) {
+
+	// Local Declarations
+	int rank;
+
+	// Check the command line arguments.
+	// Skip the executable name before parsing.
+	argc -= 1; // one for the executable name
+	argv += 1; // one for the executable name
+	Options opts;
+	opts.readParams(argc, argv);
+	if (!opts.shouldRun()) {
+		return opts.getExitCode();
+	}
+
+	// Skip the name of the parameter file that was just used.
+	// The arguments should be empty now.
+	argc -= 1;
+	argv += 1;
+
+	// Extract the argument for the file name
+	std::string networkFilename = opts.getNetworkFilename();
+	assert(!networkFilename.empty());
+
+	try {
+		// Set up our performance data infrastructure.
+        xperf::initialize(opts.getPerfHandlerType());
+
+		// Initialize MPI. We do this instead of leaving it to some
+		// other package (e.g., PETSc), because we want to avoid problems
+		// with overlapping Timer scopes.
+		MPI_Init(&argc, &argv);
+
+		// Get the MPI rank
+		MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+		if (rank == 0) {
+			// Print the start message
+			printStartMessage();
+		}
+
+		// Set up the material infrastructure that is used to calculate flux
+		auto materialInitOK = initMaterial(opts);
+		// Set up the temperature infrastructure
+		auto tempInitOK = initTemp(opts);
+		// Set up the visualization infrastructure.
+		auto vizInitOK = initViz(opts.useVizStandardHandlers());
+
+		// Access the material handler registry to get the material
+		auto materialHandler = xolotlSolver::getMaterialHandler();
+		// Access the temperature handler registry to get the temperature
+		auto tempHandler = xolotlSolver::getTemperatureHandler(opts);
+
+		// Access our performance handler registry to obtain a Timer
+		// measuring the runtime of the entire program.
+		// NOTE: these long template types could be replaced with 'auto'
+		auto handlerRegistry = xolotlPerf::getHandlerRegistry();
+		auto totalTimer = handlerRegistry->getTimer("total");
+		totalTimer->start();
+
+		// Setup the solver
+		auto solver = setUpSolver(handlerRegistry, opts.getPetscArgc(),
+				opts.getPetscArgv());
+
+		// Load the network
+		auto networkLoadTimer = handlerRegistry->getTimer("loadNetwork");
+		networkLoadTimer->start();
+
+		// Set up the network loader
+		auto networkLoader = setUpNetworkLoader(rank, MPI_COMM_WORLD,
+				networkFilename, handlerRegistry);
+
+		// Give the network loader to PETSc as input
+		solver->setNetworkLoader(networkLoader);
+		networkLoadTimer->stop();
+
+		// Launch the PetscSolver
+		launchPetscSolver(solver, handlerRegistry, materialHandler,
+				tempHandler, opts.getStepSize());
+
+		// Finalize our use of the solver.
+		auto solverFinalizeTimer = handlerRegistry->getTimer("solverFinalize");
+		solverFinalizeTimer->start();
+		solver->finalize();
+		solverFinalizeTimer->stop();
+
+		totalTimer->stop();
+
+        // Report statistics about the performance data collected during
+        // the run we just completed.
+        xperf::PerfObjStatsMap<xperf::ITimer::ValType> timerStats;
+        xperf::PerfObjStatsMap<xperf::IEventCounter::ValType> counterStats;
+        xperf::PerfObjStatsMap<xperf::IHardwareCounter::CounterType> hwCtrStats;
+        handlerRegistry->collectStatistics( timerStats, counterStats, hwCtrStats );
+        if( rank == 0 )
+        {
+            handlerRegistry->reportStatistics( std::cout, 
+                                                timerStats, 
+                                                counterStats, 
+                                                hwCtrStats );
+        }
+
+    } catch (std::exception& e ) {
+
+        std::cerr << e.what() << std::endl;
+        std::cerr << "Aborting." << std::endl;
+        return EXIT_FAILURE;
+
+	} catch (std::string & error) {
+		std::cout << error << std::endl;
+		std::cout << "Aborting." << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	// finalize our use of MPI
+	MPI_Finalize();
+
+	return EXIT_SUCCESS;
+}
