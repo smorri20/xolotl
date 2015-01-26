cc1cab4231599322fde785a64f16e909fd661de0
Sophie Blondel <sophie.blondel1@gmail.com>
2015-01-26 20:58:25 +0000

    Adding a new test for the PetscSolver2D handler. SB 20150126
    git-svn-id: file:///Users/MacAir/Dropbox/xolotl-cleaned/branches/petscsolver_branch@790 72105f29-0fe1-43bd-9363-b1e281e962a2

--- xolotl/tests/solver/PetscSolverTester.cpp
+++ xolotl/tests/solver/PetscSolverTester.cpp
@@ -16,6 +16,7 @@
 #include <HDF5NetworkLoader.h>
 #include <Options.h>
 #include <PetscSolver1DHandler.h>
+#include <PetscSolver2DHandler.h>
 #include <IMaterialFactory.h>
 #include <TemperatureHandlerFactory.h>
 #include <VizHandlerRegistryFactory.h>
@@ -108,6 +109,91 @@ BOOST_AUTO_TEST_CASE(checkPetscSolver1DHandler) {
     BOOST_REQUIRE_CLOSE(concs[1], -5.872e-29, 0.01);
     BOOST_REQUIRE_CLOSE(concs[2], -1.653e-41, 0.01);
     BOOST_REQUIRE_CLOSE(concs[7], 1.4966e-103, 0.01);
+    BOOST_REQUIRE_CLOSE(concs[8], 0.02500, 0.01);
 }
+
+ /**
+  * This operation checks the concentration of clusters after solving a test case
+  * in 2D.
+  */
+ BOOST_AUTO_TEST_CASE(checkPetscSolver2DHandler) {
+
+ 	// Initialize MPI for HDF5
+ 	int argc = 0;
+ 	char **argv;
+ 	MPI_Init(&argc, &argv);
+
+ 	// Local Declarations
+ 	string sourceDir(XolotlSourceDirectory);
+
+ 	// Create a fake command line to read the options
+ 	argc = 1;
+ 	argv = new char*[2];
+ 	std::string parameterFile = sourceDir + "/tests/testfiles/param_good_2D.txt";
+ 	argv[0] = new char[parameterFile.length() + 1];
+ 	strcpy(argv[0], parameterFile.c_str());
+ 	argv[1] = 0; // null-terminate the array
+
+ 	// Read the options
+ 	Options opts;
+ 	opts.readParams(argc, argv);
+
+ 	// Create the network loader
+ 	std::shared_ptr<HDF5NetworkLoader> loader = std::make_shared<HDF5NetworkLoader>(
+ 			make_shared<xolotlPerf::DummyHandlerRegistry>());
+
+ 	// Create the path to the network file
+ 	string pathToFile("/tests/testfiles/tungsten_diminutive_2D.h5");
+ 	string networkFilename = sourceDir + pathToFile;
+
+ 	BOOST_TEST_MESSAGE(
+ 			"PetscSolverTester Message: Network filename is: " << networkFilename);
+
+ 	// Give the filename to the network loader
+ 	loader->setFilename(networkFilename);
+
+ 	// Create the solver
+ 	std::shared_ptr<xolotlSolver::PetscSolver> solver = std::make_shared<
+ 			xolotlSolver::PetscSolver>(make_shared<xolotlPerf::DummyHandlerRegistry>());
+
+ 	// Create the material factory
+ 	auto materialFactory = xolotlFactory::IMaterialFactory::createMaterialFactory(opts.getMaterial(),
+ 			opts.getDimensionNumber());
+
+ 	// Initialize and get the temperature handler
+ 	bool tempInitOK = xolotlFactory::initializeTempHandler(opts);
+ 	auto tempHandler = xolotlFactory::getTemperatureHandler();
+
+ 	// Set up our dummy performance and visualization infrastructures
+     xolotlPerf::initialize(xolotlPerf::toPerfRegistryType("dummy"));
+     xolotlFactory::initializeVizHandler(false);
+
+     // Create a solver handler and initialize it
+ 	auto solvHandler = std::make_shared<xolotlSolver::PetscSolver2DHandler> ();
+ 	solvHandler->initializeHandlers(materialFactory, tempHandler, opts);
+
+     // Set the solver command line to give the PETSc options and initialize it
+     solver->setCommandLineOptions(opts.getPetscArgc(), opts.getPetscArgv());
+ 	solver->initialize(solvHandler);
+
+ 	// Give it the network loader
+ 	solver->setNetworkLoader(loader);
+
+ 	// Solve and finalize
+ 	solver->solve();
+ 	solver->finalize();
+
+ 	// Check the concentrations left in the network
+ 	auto network = solvHandler->getNetwork();
+ 	double concs[network->getAll()->size()];
+ 	network->fillConcentrationsArray(concs);
+
+ 	// Check some concentrations
+     BOOST_REQUIRE_CLOSE(concs[0], -7.1660e-92, 0.01);
+     BOOST_REQUIRE_CLOSE(concs[1], 4.8330e-181, 0.01);
+     BOOST_REQUIRE_CLOSE(concs[6], 4.7962e-12, 0.01);
+     BOOST_REQUIRE_CLOSE(concs[14], 0.0, 0.01);
+     BOOST_REQUIRE_CLOSE(concs[23], 5.7314e-97, 0.01);
+ }
 
 BOOST_AUTO_TEST_SUITE_END()
