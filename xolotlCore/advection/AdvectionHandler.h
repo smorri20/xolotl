25365f19ac8f250d19356c9284e70bb040328b51
dqwu <dqwu@mycompany.com>
2015-02-09 23:43:41 +0000

    Renaming the space parameter h to hx for the advection handler. DW 20150209
    git-svn-id: file:///Users/MacAir/Dropbox/xolotl-cleaned/branches/petscsolver_branch/xolotl@809 72105f29-0fe1-43bd-9363-b1e281e962a2

--- xolotlCore/advection/AdvectionHandler.h
+++ xolotlCore/advection/AdvectionHandler.h
@@ -38,42 +38,42 @@ public:
 
 	/**
 	 * Compute the flux due to the advection for all the cluster,
-	 * given the space parameter h and the position.
+	 * given the space parameter hx and the position.
 	 * This method is called by the RHSFunction from the PetscSolver.
 	 *
 	 * If D is the diffusion coefficient, and C_r, C_m the right and middle concentration
 	 * of this cluster, A the sink strength, K the Boltzmann constant, T the temperature,
 	 * the value to add to the updated concentration is:
 	 *
-	 * [(3 * A * D) / (K * T * h)] * [(C_r / [pos_x + h]^4) - (C_m / (pos_x)^4)]
+	 * [(3 * A * D) / (K * T * hx)] * [(C_r / [pos_x + hx]^4) - (C_m / (pos_x)^4)]
 	 *
 	 * @param network The network
-	 * @param h The space parameter, here the grid step size
+	 * @param hx The space parameter, here the grid step size in the x direction
 	 * @param pos The position on the grid
 	 * @param concVector The pointer to the pointer of arrays of concentration at middle,
 	 * left, and right grid points
 	 * @param updatedConcOffset The pointer to the array of the concentration at the grid
 	 * point where the advection is computed used to find the next solution
 	 */
-	void computeAdvection(PSIClusterReactionNetwork *network, double h,
+	void computeAdvection(PSIClusterReactionNetwork *network, double hx,
 			std::vector<double> &pos, double **concVector, double *updatedConcOffset);
 
 	/**
 	 * Compute the partials due to the advection of all the clusters given
-	 * the space parameter h and the position.
+	 * the space parameter hx and the position.
 	 * This method is called by the RHSJacobian from the PetscSolver.
 	 *
 	 * The partial derivative on the right grid point is given by (same notation as for
 	 * the computeAdvection method)
 	 *
-	 * (3 * A * D) / [K * T * h * (pos_x + h)^4]
+	 * (3 * A * D) / [K * T * hx * (pos_x + hx)^4]
 	 *
 	 * and on this grid point we have
 	 *
-	 * - (3 * A * D) / [K * T * h * (pos_x)^4]
+	 * - (3 * A * D) / [K * T * hx * (pos_x)^4]
 	 *
 	 * @param network The network
-	 * @param h The space parameter, here the grid step size
+	 * @param hx The space parameter, here the grid step size in the x direction
 	 * @param val The pointer to the array that will contain the values of partials
 	 * for the advection
 	 * @param indices The pointer to the array that will contain the indices of the
@@ -81,7 +81,7 @@ public:
 	 * @param pos The position on the grid
 	 */
 	void computePartialsForAdvection(PSIClusterReactionNetwork *network,
-			double h, double *val, int *indices, std::vector<double> &pos);
+			double hx, double *val, int *indices, std::vector<double> &pos);
 
 	/**
 	 * Get the total number of advecting clusters in the network.
