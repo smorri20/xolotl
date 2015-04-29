3671a3109b898a632135dc6e053c65f53a386307
Sophie Blondel <sophie.blondel1@gmail.com>
2015-04-29 15:38:04 +0000

    Updating the petscsolver branch with the new unit tests from the trunk. Fixing SeriesPlot.cpp . SB 20150409
    git-svn-id: file:///Users/MacAir/Dropbox/xolotl-cleaned/branches/petscsolver_branch@866 72105f29-0fe1-43bd-9363-b1e281e962a2

--- xolotl/xolotlViz/standard/plot/SeriesPlot.cpp
+++ xolotl/xolotlViz/standard/plot/SeriesPlot.cpp
@@ -13,7 +13,7 @@ using namespace xolotlViz;
 #define W_WIDTH 1024
 #define W_HEIGHT 1024
 
-SeriesPlot::SeriesPlot(const std::string& name) : Plot(name) :
+SeriesPlot::SeriesPlot(const std::string& name) : Plot(name),
 	plotDataProviders(std::make_shared< std::vector< std::shared_ptr<IDataProvider> > > ()) {}
 
 SeriesPlot::~SeriesPlot() {
