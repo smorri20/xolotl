ac3a0fa235227ded4476b8aba88bb8f01918c5cc
Sophie Blondel <sophie.blondel1@gmail.com>
2016-08-26 12:40:02 +0000

    Creating a new branch to start working on the new xenon test case. SB 20160826
    git-svn-id: file:///Users/MacAir/Dropbox/xolotl-cleaned/branches/xenon_branch@1040 72105f29-0fe1-43bd-9363-b1e281e962a2

---
+++ xolotl/tests/reactants/SimpleReactionNetwork.h
@@ -0,0 +1,52 @@
+#ifndef SIMPLEREACTIONNETWORK_H_
+#define SIMPLEREACTIONNETWORK_H_
+
+#include <PSIClusterReactionNetwork.h>
+#include <xolotlPerf.h>
+#include <DummyHandlerRegistry.h>
+
+namespace testUtils {
+
+/**
+ * This class creates a simple reaction network used for testing. It contains
+ * 10 each of He, V and I clusters and twenty HeV clusters. The HeV clusters are
+ * stored in ascending order of He and then V, (1,1;2,1;2,2;3,2;3,3;etc.).
+ *
+ * It does not register itself as the ReactionNetwork for its clusters because
+ * of limitations with shared_ptrs and "this." So, the
+ * TestUtils::getSimpleReactionNetwork() operation should always be called to
+ * insure that it is properly initialized.
+ */
+class SimpleReactionNetwork: public xolotlCore::PSIClusterReactionNetwork {
+
+public:
+	/**
+	 * Constructor
+	 *
+	 * @param maxClusterSize the maximal size of the clusters that will be in
+	 * the network. Set to 10 by default.
+	 * @param registry The dummy handler registry by default
+	 */
+	SimpleReactionNetwork(const int maxClusterSize = 10,
+			std::shared_ptr<xolotlPerf::IHandlerRegistry> registry = std::make_shared<xolotlPerf::DummyHandlerRegistry>());
+
+	//! Destructor
+	virtual ~SimpleReactionNetwork() {}
+
+};
+
+/**
+ * This operation creates a SimpleReactionNetwork and makes sure that it is
+ * properly registered with the clusters it contains. This operation should
+ * always be called instead of constructing a SimpleReactionNetwork manually.
+ *
+ * @param maxClusterSize the maximal size of the clusters that will be in
+ * the network. Set to 10 by default.
+ * @param registry The dummy handler registry by default
+ * @return The reaction network.
+ */
+std::shared_ptr<xolotlCore::ReactionNetwork> getSimpleReactionNetwork(const int maxClusterSize = 10,
+		std::shared_ptr<xolotlPerf::IHandlerRegistry> registry = std::make_shared<xolotlPerf::DummyHandlerRegistry>());
+
+} /* end namespace testUtils */
+#endif
---
+++ xolotl/xolotlCore/reactants/psiclusters/PSIClusterNetworkLoader.cpp
@@ -0,0 +1,145 @@
+/*
+ * PSIClusterNetworkLoader.cpp
+ *
+ *  Created on: Mar 30, 2013
+ *      Author: jaybilly
+ */
+
+#include "PSIClusterNetworkLoader.h"
+#include <TokenizedLineReader.h>
+#include <stdio.h>
+#include <limits>
+#include <algorithm>
+#include <vector>
+#include <HeCluster.h>
+#include <VCluster.h>
+#include <InterstitialCluster.h>
+#include <HeVCluster.h>
+// #include <HeInterstitialCluster.h>
+#include "PSIClusterReactionNetwork.h"
+#include <xolotlPerf.h>
+
+using namespace xolotlCore;
+
+/**
+ * This operation converts a string to a double, taking in to account the fact
+ * that the input file may contain keys such as "infinite."
+ *
+ * @param inString the string to be converted
+ * @return the string as a double
+ */
+static inline double convertStrToDouble(const std::string& inString) {
+	return (inString.compare("infinite") == 0) ?
+			std::numeric_limits<double>::infinity() :
+			strtod(inString.c_str(), NULL);
+}
+
+std::shared_ptr<PSICluster> PSIClusterNetworkLoader::createCluster(int numHe,
+		int numV, int numI) {
+	// Local Declarations
+	std::shared_ptr<PSICluster> cluster;
+
+	// Determine the type of the cluster given the number of each species.
+	// Create a new cluster by that type and specify the names of the
+	// property keys.
+	if (numHe > 0 && numV > 0) {
+		// Create a new HeVCluster
+		cluster = std::make_shared<HeVCluster>(numHe, numV, handlerRegistry);
+	}
+	else if (numHe > 0 && numI > 0) {
+		throw std::string("HeliumInterstitialCluster is not yet implemented.");
+		// FIXME! Add code to add it to the list
+	}
+	else if (numHe > 0) {
+		// Create a new HeCluster
+		cluster = std::make_shared<HeCluster>(numHe, handlerRegistry);
+	}
+	else if (numV > 0) {
+		// Create a new VCluster
+		cluster = std::make_shared<VCluster>(numV, handlerRegistry);
+	}
+	else if (numI > 0) {
+		// Create a new ICluster
+		cluster = std::make_shared<InterstitialCluster>(numI, handlerRegistry);
+	}
+
+	return cluster;
+}
+
+PSIClusterNetworkLoader::PSIClusterNetworkLoader(
+		const std::shared_ptr<std::istream> stream,
+		std::shared_ptr<xolotlPerf::IHandlerRegistry> registry) :
+	handlerRegistry(registry) {
+	setInputstream(stream);
+
+	return;
+}
+
+void PSIClusterNetworkLoader::setInputstream(
+		const std::shared_ptr<std::istream> stream) {
+	networkStream = stream;
+
+	return;
+}
+
+std::shared_ptr<PSIClusterReactionNetwork> PSIClusterNetworkLoader::load() {
+	// Local Declarations
+	TokenizedLineReader<std::string> reader;
+	std::vector<std::string> loadedLine;
+	std::shared_ptr<PSIClusterReactionNetwork> network = std::make_shared<
+			PSIClusterReactionNetwork>(handlerRegistry);
+
+	std::string error(
+			"PSIClusterNetworkLoader Exception: Insufficient or erroneous data.");
+	int numHe = 0, numV = 0, numI = 0;
+	double formationEnergy = 0.0, migrationEnergy = 0.0;
+	double diffusionFactor = 0.0;
+	std::vector<std::shared_ptr<Reactant> > reactants;
+
+	// Load the network if the stream is available
+	if (networkStream != NULL) {
+		// Load the stream
+		reader.setInputStream(networkStream);
+
+		// Loop over each line of the file, which should each be PSIClusters.
+		loadedLine = reader.loadLine();
+		while (loadedLine.size() > 0) {
+			// Check the size of the loaded line
+			if (loadedLine.size() < 6)
+				// And notify the calling function if the size is insufficient
+				throw error;
+			// Load the sizes
+			if (loadedLine[0][0] != '#') {
+				numHe = std::stoi(loadedLine[0]);
+				numV = std::stoi(loadedLine[1]);
+				numI = std::stoi(loadedLine[2]);
+				// Create the cluster
+				auto nextCluster = createCluster(numHe, numV, numI);
+				// Load the energies
+				formationEnergy = convertStrToDouble(loadedLine[3]);
+				migrationEnergy = convertStrToDouble(loadedLine[4]);
+				diffusionFactor = convertStrToDouble(loadedLine[5]);
+				// Set the formation energy
+				nextCluster->setFormationEnergy(formationEnergy);
+				// Set the diffusion factor and migration energy
+				nextCluster->setMigrationEnergy(migrationEnergy);
+				nextCluster->setDiffusionFactor(diffusionFactor);
+				// Add the cluster to the network
+				network->add(nextCluster);
+				// Add it to the list so that we can set the network later
+				reactants.push_back(nextCluster);
+			}
+
+			// Load the next line
+			loadedLine = reader.loadLine();
+		}
+
+		// Set the network for all of the reactants. This MUST be done manually.
+		for (auto reactantsIt = reactants.begin();
+				reactantsIt != reactants.end(); ++reactantsIt) {
+			(*reactantsIt)->setReactionNetwork(network);
+		}
+	}
+
+	return network;
+}
