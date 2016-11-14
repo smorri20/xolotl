49c1e5dc96094827e2acfd4fdf6a0efb323a26e3
ocekmer <ocekmer@mycompany.com>
2016-11-14 11:57:40 +0000

    Missing header was added. OC 20161114
    git-svn-id: file:///Users/MacAir/Dropbox/xolotl-cleaned/branches/uq_branch@1122 72105f29-0fe1-43bd-9363-b1e281e962a2

--- xolotl/xolotlCore/flux/FitFluxHandler.h
+++ xolotl/xolotlCore/flux/FitFluxHandler.h
@@ -3,6 +3,7 @@
 
 #include "FluxHandler.h"
 #include <cmath>
+#include <fstream>
 
 namespace xolotlCore {
 
@@ -25,19 +26,21 @@ private:
 		double p[2];	// Weibull parameters
 
 
-		// Uncomment to enter the Weibull parameters for the selected surface orientation
-		p[0] = 1.394689526243859;
-		p[1] = 1.877732240485492;
+//		// Uncomment to enter the Weibull parameters for the selected surface orientation
+//		p[0] = 1.415022317091653;
+//		p[1] = 1.918091190461391;
+//		// Compute the Weibull fit
+//		double value = p[0]/p[1] * pow(x/p[1],(p[0]-1.0)) * exp(-pow(x/p[1],p[0]));
 
-//		// Uncomment to read the Weibull parameters from a file
-//		ifstream parameters;
-//		parameters.open("/home/ocekmer/Workspaces/UQTk_Xolotl/UQTk/Step2_SurrogateConstruction/FitParameters.dat");  // Location of the Weibull-parameter file.
-//		double p[2];
-//		for (int i=0;i<2;i++)
-//			parameters >> p[i];
 
+		// Uncomment to read the Weibull parameters from a file
+		ifstream parameters;
+		parameters.open("/home/ocekmer/Workspaces/Sourceforge/uqtkXolotl/Step2_SurrogateConstruction/FitParameters.dat");  // Location of the Weibull-parameter file.
+		for (int i=0;i<2;i++)
+			parameters >> p[i];
 		// Compute the Weibull fit
 		double value = p[0]/p[1] * pow(x/p[1],(p[0]-1.0)) * exp(-pow(x/p[1],p[0]));
+		parameters.close();
 
 		return value;
 
