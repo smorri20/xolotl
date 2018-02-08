52093564883303bdda1044c8b352538fa3bab5a9
Philip C. Roth <rothpc@ornl.gov>
2018-02-08 15:54:45 -0500

    Updated cmake support for finding/configuring Kokkos.

--- xolotl/cmake-modules/FindKokkos.cmake
+++ xolotl/cmake-modules/FindKokkos.cmake
@@ -38,3 +38,49 @@ find_package_handle_standard_args(Kokkos DEFAULT_MSG
     Kokkos_INCLUDE_DIRS 
 )
 
+# We need to know some of the configuration needed to support
+# whichever Kokkos backend(s) we might use.
+if(EXISTS ${KOKKOS_ROOT}/kokkos.cmake)
+    # Older Kokkos revision (< 2.5)
+    include("${KOKKOS_ROOT}/kokkos.cmake")
+elseif(EXISTS ${KOKKOS_ROOT}/kokkos_generated_settings.cmake)
+    # Newer Kokkos revision (2.5+)
+    include("${KOKKOS_ROOT}/kokkos_generated_settings.cmake")
+endif()
+
+# * Serial, nothing else should be needed for our configuration.
+if("${KOKKOS_DEVICES}" MATCHES "Serial")
+    message(STATUS "Detected Kokkos support for Serial")
+endif()
+
+# For pthreads, we may need to add a pthreads library but shouldn't
+# need to add anything to CXXFLAGS.
+if("${KOKKOS_DEVICES}" MATCHES "Pthread")
+    message(STATUS "Detected Kokkos support for Pthread")
+endif()
+
+# For OpenMP, we use CMake's support for figuring out what it
+# needs to build OpenMP code rather than taking it from whatever
+# configuration we used to configure Kokkos.
+if("${KOKKOS_DEVICES}" MATCHES "OpenMP")
+    message(STATUS "Detected Kokkos support for OpenMP.  Finding OpenMP.")
+    find_package(OpenMP REQUIRED)
+endif()
+
+# For CUDA, we probably need to use whatever was used to configure Kokkos.
+if("${KOKKOS_DEVICES}" MATCHES "Cuda")
+    message(STATUS "Detected Kokkos support for CUDA")
+    # The flags we used when building Kokkos probably have a C++
+    # standard selection flag (e.g., -std=c++11).  We probably set this
+    # already ourself, so to avoid compiler warnings, we strip it.
+    string(REGEX REPLACE "--*std=[^ ]*" "" MY_KOKKOS_CXXFLAGS ${KOKKOS_CXXFLAGS})
+    set(KOKKOS_CXXFLAGS "${MY_KOKKOS_CXXFLAGS}")
+
+    # We assume that the user is compiling with nvcc_wrapper,
+    # which adds the appropriate linker flags and libraries for us.
+endif()
+
+# For std::threads (once it is supported), nothing should be
+# needed beyond whatever C++ standards flag we give.
+
+
